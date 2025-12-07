e8fab70f7ee19f9561a8c70c432a2ffab8e210a9
inescodefam <ines.codefam@gmail.com>
2025-12-07 18:55:27 +0100

    Implement goal cntroller

--- CareerTrack/Program.cs
+++ CareerTrack/Program.cs
@@ -25,4 +25,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated(); 
+}
+
 app.Run();
