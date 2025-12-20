e2542049897558476a61dcdafa38697fbb443cd0
inescodefam <ines.codefam@gmail.com>
2025-12-20 11:53:09 +0100

    add

--- CareerTrack/Services/ExporterData/ExcelExporter.cs
+++ CareerTrack/Services/ExporterData/ExcelExporter.cs
@@ -2,15 +2,20 @@
 {
     // open / close principle
 
-    public class ExcelExporter
+    public class ExcelExporter : IExporter
     {
         public string FormatName => "Excel";
         public string ContentType => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
-        public byte[] Export(ExportData data)
+        public byte[] Export(ExportData data) // kako bi bilo da nije ISP
         {
             var csv = GenerateCsvContent(data);
             return System.Text.Encoding.UTF8.GetBytes(csv);
+        }
+
+        public byte[] Export(IExportUserData userData, IExportGoalData goalData)
+        {
+            throw new NotImplementedException();
         }
 
         private string GenerateCsvContent(ExportData data)
