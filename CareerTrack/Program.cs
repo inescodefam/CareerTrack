7f0dc04c7f81cc4ef08a973bef755c546a275ecf
inescodefam <ines.codefam@gmail.com>
2026-01-17 19:30:31 +0100

    [CT-174] Resolve SonarCube issues for Program.cs

--- CareerTrack/Program.cs
+++ CareerTrack/Program.cs
@@ -12,13 +12,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//ensure ModelState check
 builder.Services.Configure<ApiBehaviorOptions>(options =>
     options.SuppressModelStateInvalidFilter = true);
 
@@ -27,32 +25,8 @@ builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAuthCookieService, AuthCookieService>();
-//builder.Services.AddScoped<IAuthService, AuthService>();
-
-//builder.Services.AddScoped<ILoginService, ILoginService>();
-//builder.Services.AddScoped<IRegistrationService, IRegistrationService>();
-//builder.Services.AddScoped<ILogoutService, ILogoutService>();
-
 
 builder.Services.AddScoped<IRoleResolver, DefaultRoleResolver>();
-
-//OCP demonstracija
-//builder.Services.AddScoped<IRoleResolver, PremiumRoleResolver>();
-
-//builder.Services.AddScoped<IRoleResolver, BadRoleResolver>();
-
-
-//cookie auth
-//builder.Services.AddAuthentication()
-//  .AddCookie(options =>
-//  {
-//      options.LoginPath = "/User/Login";
-//      options.LogoutPath = "/User/Logout";
-//      options.AccessDeniedPath = "/User/Forbidden";
-//      options.SlidingExpiration = true;
-//      options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-//  });
-
 builder.Services
     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
@@ -89,22 +63,15 @@ builder.Services.AddHsts(options =>
     options.MaxAge = TimeSpan.FromDays(365);
 });
 
-
-// http context
 builder.Services.AddHttpContextAccessor();
-
-// register:
-// Services
 builder.Services.AddScoped<IGoalService, GoalService>();
 builder.Services.AddScoped<IProgressService, ProgressService>();
 builder.Services.AddScoped<IUserContextService, UserContextService>();
 builder.Services.AddScoped<IGoalExportService, GoalExportService>();
 builder.Services.AddScoped<IGoalFactory, GoalFactory>();
 
-// Repositories
 builder.Services.AddScoped<IGoalRepository, GoalRepository>();
 
-// Utilities
 builder.Services.AddScoped<IDateTimeConverter, DateTimeConverter>();
 
 
@@ -118,19 +85,14 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
 
 app.UseHttpsRedirection();
-// with extension method - oæu da izgleda ko ostale :)
 app.UseSecurityHeaders();
 app.UseStaticFiles();
-// register middleware
-//app.UseMiddleware<SecurityHeadersMiddleware>();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
@@ -142,10 +104,8 @@ app.MapControllerRoute(
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated();
+    await context.Database.EnsureCreatedAsync();
 }
 
-app.Run();
-
-
-public partial class Program { }
+await app.RunAsync();
+public static partial class Program { }
